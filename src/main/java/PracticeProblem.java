63a32086b04184e12af26d38343320ff6a35ae62
Joe-Yang1 <348993072@tdsb.ca>
2026-03-30 15:15:45 +0000

    Done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,38 @@
+/**
+ * File: If statements
+ * Author: Joe Yang
+ * Date Created: Mar 23, 2026
+ * Date Last Modified: Mar 23, 2026
+ */
+
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static void main(String args[]){}
+	
+	public static String evenOrOdd(int num){
+    if (num % 2 == 0){
+        return ("Even");
+    }
+        return ("Odd");
+    }
 
-	}
 
+public static String teacherOrStudent(String user){
+    if (user.equals("Kalisz")){
+        return "Teacher";
+    }
+
+        return "Student";
+
+}
+
+public static int fartherFromZero(int user2){
+    if (user2 > 0 ){
+        return user2 + 5;
+    }
+    if (user2 < 0){
+        return (user2 - 5);
+    }
+        return (user2);
+}
 }
